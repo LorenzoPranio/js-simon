2e9ba09f124891dfef25b8fb5abcfb61881d6cc4
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-09 15:33:13 +0200

    [UPD]Added scaffolding in script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Creare un ARRAY vuoto che conterrà i 5 numeri casuali
+
+//Creare un SetTimeout di 30 secondi per far sparire i numeri dallo schermo
+
+//Creo un prompt per chiedere all'utente di inserire i numeri visti su schermo precedentemente
+
+//Creare una funzione per i numeri casuali
+
+//Creo una funzione per verificare se i numeri scritti dall'utente sono presenti nell'array
