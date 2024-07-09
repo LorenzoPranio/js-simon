3c5a6cb77ecd0162c1b1d2ae1ecec7cf3599e8b3
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-09 16:20:24 +0200

    [UPD]Added timeout, prompt and created function for verify numbers in prompt in script.js

--- js/script.js
+++ js/script.js
@@ -21,6 +21,36 @@ random_numbers();
 //I numeri devono essere mostrati su schermo
 document.getElementById('numeri').innerText = numeri_casuali.join(', ');
 
-//Creare un SetTimeout di 30 secondi per far sparire i numeri dallo schermo
 
-//Creo un prompt per chiedere all'utente di inserire i numeri visti su schermo precedentemente
+
+
+//Creare un SetTimeout di 30 secondi per far sparire i numeri dallo schermo
+setTimeout(function() {
+    document.getElementById('numeri').innerText = '';
+    //Creo un prompt per chiedere all'utente di inserire i numeri visti su schermo precedentemente
+    let numeri_utente = prompt("Inserisci i numeri che hai visto: (Es: 1, 2, 3...)");
+    
+    //Creo una funzione per verificare se i numeri scritti dall'utente sono presenti nell'array
+    function verifica_numeri(numeri_utente) {
+        let arr_numeri_utente = numeri_utente.split(',').map(Number);
+        let numeri_corretti = true;
+
+        //IF l'elemento è presente nell'array
+        for (let num of arr_numeri_utente) {
+            if (!numeri_casuali.includes(num)) {
+                numeri_corretti = false;
+            }
+        }
+        //In console log: "I numeri inseriti sono corretti"
+        if (numeri_corretti) {
+            console.log("I numeri inseriti sono corretti");
+        } else {
+            //In console log: "I numeri inseriti non sono corretti"
+            console.log("I numeri inseriti non sono corretti");
+        }
+    }
+    // Verifica i numeri inseriti dall'utente
+    verifica_numeri(numeri_utente);
+
+}, 3000);
+
