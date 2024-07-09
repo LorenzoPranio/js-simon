7bdd9654a17fcc5a46e58bcd85468145740ff1e1
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-09 15:58:11 +0200

    [UPD]Added numbers on screen and fixed function random numbers in script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
 //Creare un ARRAY vuoto che conterrà i 5 numeri casuali
 
 let numeri_casuali = [];
+console.log(numeri_casuali)
 
-//Creare un SetTimeout di 30 secondi per far sparire i numeri dallo schermo
-
-//Creo un prompt per chiedere all'utente di inserire i numeri visti su schermo precedentemente
+document.getElementById('numeri').innerText = numeri_casuali.join(', ');
 
 //Creare una funzione per i numeri casuali
 
 function random_numbers(){
-    while (random_numbers.length < 5){
-        let num = Math.floor(Math.random() * 100 + 1);
-        if (!random_numbers.includes(num)){
-            random_numbers.push(num)
+    while (numeri_casuali.length < 5){
+    let num = Math.floor(Math.random() * 100 + 1);
+        if (!numeri_casuali.includes(num)){
+            numeri_casuali.push(num)
         }
     }
 }
 
-//Creo una funzione per verificare se i numeri scritti dall'utente sono presenti nell'array
+random_numbers();
+
+//I numeri devono essere mostrati su schermo
+document.getElementById('numeri').innerText = numeri_casuali.join(', ');
+
+//Creare un SetTimeout di 30 secondi per far sparire i numeri dallo schermo
+
+//Creo un prompt per chiedere all'utente di inserire i numeri visti su schermo precedentemente
