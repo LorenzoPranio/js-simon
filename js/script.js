89974d1a77754fe3fcc3ecdb2fe674debb464b57
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-09 15:34:20 +0200

    [UPD]Created empty array in script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 //Creare un ARRAY vuoto che conterrà i 5 numeri casuali
 
+let numeri_casuali = [];
+
 //Creare un SetTimeout di 30 secondi per far sparire i numeri dallo schermo
 
 //Creo un prompt per chiedere all'utente di inserire i numeri visti su schermo precedentemente
